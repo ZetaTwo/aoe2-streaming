33d116c18b959b68632b2f486e2197b3817af77d
Calle Svensson <calle.svensson@zeta-two.com>
2026-03-19 21:30:26 +0000

    Rendering and styling

---
+++ aoe2-groups-overlay/src/components/BracketStanding.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { BracketStanding as BracketStandingType } from '@/stores/standings'
+import GroupStandingTable from './GroupStanding.vue'
+
+defineProps<{
+  bracket: BracketStandingType
+}>()
+</script>
+
+<template>
+  <div class="bracket-standing">
+    <h2 class="bracket-title">{{ bracket.name }} Bracket</h2>
+    <div class="groups-container">
+      <GroupStandingTable
+        v-for="group in bracket.groups"
+        :key="group.name"
+        :group="group"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.groups-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.bracket-title {
+  text-align: center;
+}
+</style>
